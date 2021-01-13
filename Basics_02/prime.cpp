fd1db3194e50b9966c83da06f07649f1fb8fc481
Satish Kumar <satish.kumar06@nagarro.com>
2021-01-13 20:17:58 +0530

    Break and Continue Statement
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ Basics_02/prime.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    int i;
+    for(i=2; i<n; i++) {
+        if(n%i==0) {
+            cout<<n<<" is not a prime number"<<endl;
+            break;
+        }
+    }
+    if(i==n) {
+        cout<<n<<" is a prime number"<<endl;
+    }
+
+    return 0;
+}
