8801b7d7fa03a79aec0df4696ac53bb15b259b98
Satish Kumar <satish.kumar06@nagarro.com>
2021-01-21 10:06:54 +0530

    STRINGS
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ 12.Strings/demo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+    // string str;
+    // cin>>str;
+    // cout<<str<<endl;
+
+    // string str1(5, 'n');
+    // cout<<str1<<endl;
+
+    string st;
+    getline(cin, st);
+    cout<<st;
+
+    return 0;
+}
---
+++ 12.Strings/stringFunctions.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+    // string s1 = "fam";
+    // string s2 = "ily";
+
+    // s1.append(s2);
+    // cout<<s1<<endl;
+
+    // cout<<s1+s2<<endl;
+
+    // cout<<s1[1]<<endl;
+
+    // string abc = "something something";
+    // abc.clear();
+    // cout<<abc<<endl;
+
+    // string str1 = "abc";
+    // string str2 = "xyz";
+
+    // cout<<str2.compare(str1)<<endl;
+    // if(str1.compare("abc") == 0) {
+    //     cout<<"Strings are equal"<<endl;
+    // }
+
+    // if(!str2.empty()) {
+    //     cout<<"String is not empty"<<endl;
+    // }
+
+    // string s1 = "nincompoop";
+    // s1.erase(3,2);
+    // cout<<s1<<endl;
+    // cout<<s1.find("com")<<endl;
+    // s1.insert(2,"lol");
+    // cout<<s1<<endl;
+    // cout<<s1.length()<<endl;
+    // for(int i=0; i<s1.length(); i++) {
+    //     cout<<s1[i]<<" ";
+    // }
+    // cout<<s1.substr(6,4)<<endl;
+
+    // string str = "786";
+    // int x = stoi(str);
+    // cout<<x<<endl;
+
+    // int x = 876;
+    // cout<< to_string(x)+"2"<<endl;
+
+    string st = "xmfybzsjqahdn";
+    sort(st.begin(), st.end());
+    cout<<st<<endl;
+
+    return 0;
+
+}    
