fabbda6fee7e410be8f7e3e196c530b02d4b934f
Satish Kumar <satish.kumar06@nagarro.com>
2021-01-22 12:46:41 +0530

    String Challenges
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ 12.Strings_01/uppercaseAndLowercase.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+    string str = "ajhgsyjsdfhef";
+
+    //convert into uppercase
+    for(int i=0; i<str.size(); i++) {
+        if(str[i] >= 'a' && str[i] <='z') {
+            str[i] -= 32;
+        }
+    }
+    cout<<str<<endl;
+
+    //convert into lowercase
+    for(int i=0; i<str.size(); i++) {
+        if(str[i] >= 'A' && str[i] <='Z') {
+            str[i] += 32;
+        }
+    }
+    cout<<str<<endl;
+
+    return 0;
+}
