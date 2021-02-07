c786e05fff7a537948e779bdcb369019f3b6fde1
Satish Kumar <satish.kumar06@nagarro.com>
2021-02-07 12:01:20 +0530

    Detection and Removal of Cycle | Hare and Tortoise Algorithm | Floyd's Algorithm
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- 24.LinkedList_01/linkedlist.cpp
+++ 24.LinkedList_01/linkedlist.cpp
@@ -136,6 +136,57 @@ node* reversek(node* &head, int k) {
     return prevptr;
 }
 
+void makeCycle(node* &head, int pos) {
+
+    node* temp = head;
+    node* startNode;
+
+    int count = 1;
+    while(temp->next!=NULL) {
+        if(count==pos){
+            startNode=temp;
+        }
+        temp=temp->next;
+        count++;
+    }
+    temp->next = startNode;
+}
+
+bool detectCycle(node* &head) {
+
+    node* slow = head;
+    node* fast = head;
+
+    while(fast!=NULL && fast->next!=NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(fast==slow) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void removeCycle(node* &head) {
+
+    node* slow = head;
+    node* fast = head;
+
+    do {
+        slow = slow->next;
+        fast = fast->next->next;
+    } while(slow!=fast);
+
+    fast = head;
+    while(slow->next != fast->next) {
+        slow=slow->next;
+        fast=fast->next;
+    }
+
+    slow->next=NULL;
+}
+
 int main() {
 
     node* head = NULL;
@@ -160,10 +211,17 @@ int main() {
     insertAtTail(head,5);
     insertAtTail(head,6);
 
+    // display(head);
+    // int k=2;
+    // node* newhead = reversek(head,k);
+    // display(newhead);
+
+    makeCycle(head,3);
+    cout<<detectCycle(head)<<endl;
+
+    removeCycle(head);
+    cout<<detectCycle(head)<<endl;
     display(head);
-    int k=2;
-    node* newhead = reversek(head,k);
-    display(newhead);
 
     return 0;
 }
