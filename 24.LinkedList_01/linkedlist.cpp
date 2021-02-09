46069c2730d89fbcba9d08f40a9ccc3059b0cb8b
Satish Kumar <satish.kumar06@nagarro.com>
2021-02-09 10:02:20 +0530

    Intersection Point of 2 Linked-List
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- 24.LinkedList_01/linkedlist.cpp
+++ 24.LinkedList_01/linkedlist.cpp
@@ -225,6 +225,60 @@ node* kappend(node* &head, int k) {
     return newHead;
 }
 
+int intersection(node* &head1, node* &head2) {
+
+    int l1 = length(head1);
+    int l2 = length(head2);
+
+    int d=0;
+    node* ptr1;
+    node* ptr2;
+
+    if(l1>l2) {
+        d=l1-l2;
+        ptr1=head1;
+        ptr2=head2;
+    }
+    else {
+        d=l2-l1;
+        ptr1=head2;
+        ptr2=head1;
+    }
+
+    while(d) {
+        ptr1=ptr1->next;
+        if(ptr1==NULL) {
+            return -1;
+        }
+        d--;
+    }
+
+    while(ptr1 != NULL && ptr2 != NULL) {
+        if(ptr1==ptr2) {
+            return ptr1->data;
+        }
+        ptr1=ptr1->next;
+        ptr2=ptr2->next;
+    }
+
+    return -1;
+}
+
+void intersect(node* &head1, node* &head2, int pos) {
+
+    node* temp1 = head1;
+    pos--;
+    while(pos--) {
+        temp1 = temp1->next;
+    }
+    node* temp2 = head2;
+    while(temp2->next != NULL) {
+        temp2 = temp2->next;
+    }
+
+    temp2->next = temp1;
+}
+
 int main() {
 
     node* head = NULL;
@@ -261,9 +315,20 @@ int main() {
     // cout<<detectCycle(head)<<endl;
     // display(head);
 
+    // display(head);
+    // node* newhead = kappend(head,3);
+    // display(newhead);
+
+    node* head2 = NULL;
+    insertAtTail(head2,9);
+    insertAtTail(head2,10);
+
+    intersect(head,head2,3);
     display(head);
-    node* newhead = kappend(head,3);
-    display(newhead);
+    display(head2);
+
+    cout<<intersection(head,head2)<<endl;
+    
 
     return 0;
 }
